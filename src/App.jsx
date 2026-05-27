ab326ae598eaf3a44729103eef8196f61b76182c
Atrevete <jr.romero.berto@gmail.com>
2026-05-27 01:10:46 -0500

    Estilos de PROD BOARD

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./componentes/Header";
+import PromoBar from "./componentes/PromoBar";
 import Hero from "./componentes/Hero";
 import Store from "./componentes/Store";
 function App() {
   return (
     <>
       <Header />
+      <PromoBar />
       <Hero />
       <Store />
     </>
---
+++ src/componentes/PromoBar.jsx
@@ -0,0 +1,28 @@
+function PromoBar(){
+    return(
+    <div className="promo-bar">
+        <div className="promo-track">
+            <span>
+                APROVECHA EL 50% DE DESCUENTO EXCLUSIVO DE LAS TIENDAS EN VELMORA
+            </span>
+            <span>
+                AFÍLIATE AHORA PARA CONSEGUIR UN MES GRATUITO PARA TU TIENDA EN VELMORA
+            </span>
+            <span>
+                DESCUBRE NUEVAS MARCAS Y PROMOCIONES EXCLUSIVAS SOLO EN VELMORA
+            </span>
+
+            <span>
+                APROVECHA EL 50% DE DESCUENTO EXCLUSIVO DE LAS TIENDAS EN VELMORA
+            </span>
+            <span>
+                AFÍLIATE AHORA PARA CONSEGUIR UN MES GRATUITO PARA TU TIENDA EN VELMORA
+            </span>
+            <span>
+                DESCUBRE NUEVAS MARCAS Y PROMOCIONES EXCLUSIVAS SOLO EN VELMORA
+            </span>
+        </div>
+    </div>
+    );
+}
+export default PromoBar;
